ba51d78218820b8c42a89508580d8455126ade89
DURGA POUDEL <durgaprasad.poudel@gmail.com>
2024-07-12 12:54:37 +0545

    correction in nepali date 2081

--- src/helpers/secondaryHelpers.tsx
+++ src/helpers/secondaryHelpers.tsx
@@ -231,13 +231,13 @@ export const calendarFunctions = {
     } else if (bsYear === 2088 && bsMonth > 5) {
       daysNumFromMinBsYear += bsDate - 4
     } else if (bsYear === 2081 && bsMonth === 2) {
-      daysNumFromMinBsYear += bsDate + 1;
+      daysNumFromMinBsYear += bsDate - 1;
     } else if (bsYear === 2081 && bsMonth === 3) {
-      daysNumFromMinBsYear += bsDate - 1;
-    } else if (bsYear === 2081 && bsMonth === 11) {
-      daysNumFromMinBsYear += bsDate - 1;
-    } else if (bsYear === 2081 && bsMonth === 12) {
       daysNumFromMinBsYear += bsDate + 1;
+    } else if (bsYear === 2081 && bsMonth === 11) {
+      daysNumFromMinBsYear += bsDate + 1;
+    } else if (bsYear === 2081 && bsMonth === 12) {
+      daysNumFromMinBsYear += bsDate - 1;
     }else {
       daysNumFromMinBsYear += bsDate
     }
